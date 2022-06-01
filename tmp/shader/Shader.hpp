58423a43b3839cb54ddc7b1124c72706e7db1ae4
Louis Raban-Schürmann <louiswilhelmrs@gmail.com>
2022-06-01 10:11:08 -0400

    tmp & gitignore

---
+++ tmp/shader/Shader.hpp
@@ -0,0 +1,39 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include "imgui.h"
+#include "GL/gl3w.h"
+#include <string>
+#include <iostream>
+
+class shader
+{
+  private:
+	const GLchar * shaderContentStr; // shader-file content
+  public:
+	const GLuint id; // shader id
+	const std::string name;
+
+	// empty constructor deprecated
+	shader() = delete;
+
+	// default constructor needs file-content and shader-type
+	shader( const std::string & name, const std::string & shaderContentStr, const GLenum & type );
+
+	// default destructor
+	~shader();
+
+	// delete shader
+	void del();
+
+	// compile shader and check compilation
+	bool compile();
+
+	// attach shader to program
+	void attachToProgram( const GLuint & programId );
+
+	// detach shader from program
+	void detachFromPorgram( const GLuint & programId );
+};
+
+#endif // !SHADER_H
---
+++ tmp/shader/ShaderManager.cpp
@@ -0,0 +1,103 @@
+#include "ShaderManager.hpp"
+#include "utils/read_file.hpp"
+
+ShaderManager::~ShaderManager()
+{
+	shaders.clear();
+	glDeleteProgram( program );
+}
+
+void ShaderManager::addShader( const std::string & name, const GLenum & type, const std::string & file )
+{
+	shaders.emplace_back( name, M3D_ISICG::readFile( folder + file ), type );
+	shaders.back().attachToProgram( program );
+}
+
+shader * ShaderManager::getShader( const std::string & name )
+{
+	for ( size_t i = 0; i < shaders.size(); i++ )
+	{
+		if ( !shaders[ i ].name.compare( name ) )
+			return &shaders[ i ];
+	}
+	std::cout << "ERROR : No shader found named " << name << std::endl;
+	return nullptr;
+}
+
+void ShaderManager::use( GLuint & prgrm )
+{
+	glUseProgram( program );
+	prgrm = program;
+}
+
+void ShaderManager::link()
+{
+	glLinkProgram( program );
+	// Check if link is ok.
+	GLint linked;
+	glGetProgramiv( program, GL_LINK_STATUS, &linked );
+	if ( !linked )
+	{
+		GLchar log[ 1024 ];
+		glGetProgramInfoLog( program, sizeof( log ), NULL, log );
+		std::cerr << "Error linking program: " << log << std::endl;
+		exit( 1 );
+	}
+}
+
+void ShaderManager::setBool( const std::string & name, bool value ) const
+{
+	glUniform1i( glGetUniformLocation( program, name.c_str() ), (int)value );
+}
+
+void ShaderManager::setInt( const std::string & name, int value ) const
+{
+	glUniform1i( glGetUniformLocation( program, name.c_str() ), value );
+}
+
+void ShaderManager::setFloat( const std::string & name, float value ) const
+{
+	glUniform1f( glGetUniformLocation( program, name.c_str() ), value );
+}
+
+void ShaderManager::setVec2( const std::string & name, const glm::vec2 & value ) const
+{
+	glUniform2fv( glGetUniformLocation( program, name.c_str() ), 1, &value[ 0 ] );
+}
+void ShaderManager::setVec2( const std::string & name, float x, float y ) const
+{
+	glUniform2f( glGetUniformLocation( program, name.c_str() ), x, y );
+}
+
+void ShaderManager::setVec3( const std::string & name, const glm::vec3 & value ) const
+{
+	glUniform3fv( glGetUniformLocation( program, name.c_str() ), 1, &value[ 0 ] );
+}
+void ShaderManager::setVec3( const std::string & name, float x, float y, float z ) const
+{
+	glUniform3f( glGetUniformLocation( program, name.c_str() ), x, y, z );
+}
+
+void ShaderManager::setVec4( const std::string & name, const glm::vec4 & value ) const
+{
+	glUniform4fv( glGetUniformLocation( program, name.c_str() ), 1, &value[ 0 ] );
+}
+void ShaderManager::setVec4( const std::string & name, float x, float y, float z, float w )
+{
+	glUniform4f( glGetUniformLocation( program, name.c_str() ), x, y, z, w );
+}
+
+void ShaderManager::setMat2( const std::string & name, const glm::mat2 & mat ) const
+{
+	glUniformMatrix2fv( glGetUniformLocation( program, name.c_str() ), 1, GL_FALSE, &mat[ 0 ][ 0 ] );
+}
+
+void ShaderManager::setMat3( const std::string & name, const glm::mat3 & mat ) const
+{
+	glUniformMatrix3fv( glGetUniformLocation( program, name.c_str() ), 1, GL_FALSE, &mat[ 0 ][ 0 ] );
+}
+
+void ShaderManager::setMat4( const std::string & name, const glm::mat4 & mat ) const
+{
+	glUniformMatrix4fv( glGetUniformLocation( program, name.c_str() ), 1, GL_FALSE, &mat[ 0 ][ 0 ] );
+}
