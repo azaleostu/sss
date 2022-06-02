dc65329f0137ff28b96675b3312efbf173c3f71d
Louis Raban-Schürmann <louiswilhelmrs@gmail.com>
2022-06-02 16:01:09 -0400

    image

--- src/utils/image.hpp
+++ src/utils/image.hpp
@@ -1,22 +1,18 @@
 #ifndef __IMAGE_HPP__
 #define __IMAGE_HPP__
 
-#include "file_path.hpp"
+#include "filePath.hpp"
 
-namespace M3D_ISICG
-{
-	struct Image
-	{
-		Image() = default;
-		~Image();
+struct Image {
+  Image() = default;
+  ~Image();
 
-		bool load( const FilePath & p_filePath );
+  bool load(const FilePath& p_filePath);
 
-		int				_width		= 0;
-		int				_height		= 0;
-		int				_nbChannels = 0;
-		unsigned char * _pixels		= nullptr;
-	};
-} // namespace M3D_ISICG
+  int _width = 0;
+  int _height = 0;
+  int _nbChannels = 0;
+  unsigned char* _pixels = nullptr;
+};
 
 #endif // __IMAGE_HPP__
